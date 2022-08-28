9f11b755aed4129b836b5915b185a18a8c7dfb75
Nyambura🌻 <bianca@NyambusMacBook.local>
2022-08-28 22:16:49 +0300

    route the app

--- rangi/src/app/app-routing.module.ts
+++ rangi/src/app/app-routing.module.ts
@@ -1,9 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {IntroComponent} from './landing-page/intro/intro.component'
+import {WhatWeDoComponent} from './landing-page/what-we-do/what-we-do.component'
+import {TestimonialsComponent} from './landing-page/testimonials/testimonials.component'
+import {CatalogueComponent } from './catalogue/catalogue.component';
+import {ThumbnailComponent } from './catalogue/thumbnail/thumbnail.component';
+import {OrderComponent } from './catalogue/order/order.component';
+import {CalculateComponent } from './catalogue/calculate/calculate.component';
+
+
 import { AuthComponent } from './auth/auth.component';
 
+
 const routes: Routes = [
-  {path:'', component:AuthComponent}
+  {path:'', component:LandingPageComponent},
+  {path: 'landingpage', component: LandingPageComponent},
+  {path: 'intro', component: IntroComponent},
+  {path: 'what-we-do', component: WhatWeDoComponent},
+  {path: 'testimonials', component: TestimonialsComponent},
+  {path: 'catalogue', component: CatalogueComponent},
+  {path: 'thumbnail', component: ThumbnailComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'calculate', component: CalculateComponent},
+  {path: 'auth', component: AuthComponent},
+
+
+
 ];
 
 @NgModule({
